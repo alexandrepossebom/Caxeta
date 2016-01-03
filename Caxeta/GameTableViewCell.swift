8d27d451d9118b096fb68987a30cff08a6b7de30
Alexandre Possebom <alexandrepossebom@gmail.com>
2016-01-03 11:30:21 -0200

    Added alert

--- Caxeta/GameTableViewCell.swift
+++ Caxeta/GameTableViewCell.swift
@@ -7,17 +7,24 @@
 //
 
 import UIKit
-    
+
 class GameTableViewCell: UITableViewCell {
     
     var player:Player?
     var gameViewController:GameViewController?
-
+    
     @IBOutlet weak var labelNome: UILabel!
     
     @IBAction func buttonWin(sender: UIButton) {
-        DAO.calcRound(player!)
-        gameViewController?.navigationController?.popViewControllerAnimated(true)
+        let alert = UIAlertController(title: "Winner", message: "\(player!.name) is the winner?", preferredStyle: UIAlertControllerStyle.Alert)
+        
+        alert.addAction(UIAlertAction(title: "No", style: .Cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Yes", style: .Destructive, handler: { (action: UIAlertAction!) in
+            DAO.calcRound(self.player!)
+            self.gameViewController?.navigationController?.popViewControllerAnimated(true)
+        }))
+        
+        gameViewController?.presentViewController(alert, animated: true, completion: nil)
     }
     
 }
